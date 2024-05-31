fcc935132f4bff04ab7b3c690b1b5759d6ce526d
AKASH KUMAR <akash30521@gmail.com>
2024-05-31 12:08:28 +0530

    Create solution.cpp

---
+++ Linked List/Lec 1/Intro to Linked List/solution.cpp
@@ -0,0 +1,37 @@
+/*class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        // code here
+        Node* head = new Node(arr[0]);
+        Node* curr = head;
+        for(int i = 1; i < arr.size(); i++){
+            curr->next = new Node(arr[i]);
+            curr = curr->next;
+        }
+        return head;
+    }
+};
+
+
+
+
