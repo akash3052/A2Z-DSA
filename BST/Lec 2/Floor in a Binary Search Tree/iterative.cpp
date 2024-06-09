ec8fab08c6850dc4d231503971fb9891efda6fb9
AKASH KUMAR <akash30521@gmail.com>
2024-06-09 17:56:51 +0530

    Create iterative.cpp

---
+++ BST/Lec 2/Floor in a Binary Search Tree/iterative.cpp
@@ -0,0 +1,23 @@
+class Solution{
+
+public:
+    int floor(Node* root, int x) {
+        if(root == NULL){
+            return -1;
+        }
+        int floor = -1;
+        while(root){
+            if(root->data == x){
+                return root->data;
+            }else if(root->data < x){
+                floor = root->data;
+                root = root->right;
+            }else{
+                root = root->left;
+            }
+        }
+        return floor;
+    }
+};
+
+
