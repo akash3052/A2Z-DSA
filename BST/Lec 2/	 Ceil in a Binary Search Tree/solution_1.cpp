a4a499c19f2b52490578b42cc50f9e0793a36cf2
AKASH KUMAR <akash30521@gmail.com>
2024-06-09 17:34:46 +0530

    Update solution_1.cpp

--- BST/Lec 2/	 Ceil in a Binary Search Tree/solution_1.cpp
+++ BST/Lec 2/	 Ceil in a Binary Search Tree/solution_1.cpp
@@ -10,6 +10,9 @@ int findCeil(Node* root, int input) {
         // If the key is grater than X , 
         // then the node can be a possible solution 
         // but to get immediate greater value we have to call the function with left subtree
+
+         // We may find a node with is larger data than input value in left subtree, 
+         // if not the root itself will be ceil node.
         int cceil = findCeil(root->left, input);
         return (cceil >= input) ? cceil : root->data;
     }
