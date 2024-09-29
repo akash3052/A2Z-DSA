f76ed87e3afabd61a150e183551f21db6df5c8b4
AKASH KUMAR <akash30521@gmail.com>
2024-09-30 00:00:55 +0530

    Update memoization_0_n-1.cpp

--- DP/1D DP/Maximum sum of non-adjacent elements (DP 5)/memoization_0_n-1.cpp
+++ DP/1D DP/Maximum sum of non-adjacent elements (DP 5)/memoization_0_n-1.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int solve(int index, int n, vector<int> &nums, vector<int> &dp){
+        if(index >= n){
+            return 0;
+        }
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        int rob = nums[index] + solve(index + 2, n, nums, dp);
+        int notrob = solve(index+1, n, nums, dp);
+        return dp[index] = max(rob, notrob);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1, -1);
+        return solve(0, n, nums, dp);
+    }
+};
+
 
