b80e6cdea890dedce6d38bba5a17f63aa7f6abcd
AKASH KUMAR <akash30521@gmail.com>
2024-12-20 22:34:15 +0530

    Create solution_1.cpp

---
+++ Recursion/Count all subsequences with sum K/solution_1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    void solve(int index, int &n, vector<int> &input, int target, int &count){
+        if(index == n){
+            if(target == 0){
+                count++;
+            }
+            return;
+        }
+        if(input[index] <= target){
+            solve(index+1, n, input, target - input[index], count);
+        }
+        solve(index+1, n, input, target, count);
+    }
+    
+    int perfectSum(vector<int>& arr, int target) {
+        // code here
+        int n = arr.size();
+        int count = 0, index = 0;
+        solve(index, n, arr, target, count);
+        return count;
+    }
+};
+
+
+// **Time complexity**: \( O(2^n) \), **Space complexity**: \( O(n) \).
